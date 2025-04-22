d26a63d8bba7d87835b17e6bd597d533140cf77f
Dmitry Elackov <dm.elackov@gmail.com>
2025-04-22 15:33:51 +0300

    First part of update to new db

--- src/deva_transcript/app.py
+++ src/deva_transcript/app.py
@@ -3,10 +3,12 @@ import time
 from faststream import FastStream, Logger
 from faststream.rabbit import RabbitBroker
 
-from deva_p1_db.repositories import TaskRepository, FileRepository
+from deva_p1_db.repositories import TaskRepository, FileRepository, ProjectRepository
 from deva_p1_db.enums.task_type import TaskType
 from deva_p1_db.enums.rabbit import RabbitQueuesToAi, RabbitQueuesToBack
 from deva_p1_db.models import Task
+from deva_p1_db.enums.file_type import FileTypes, resolve_file_type
+from openai import project
 
 from deva_transcript.database import Session
 from deva_transcript.neural.summary import create_summary, load_openai_model
@@ -36,13 +38,19 @@ working_queue = {
 
 async def task_transcribe(task_model: Task, session: Session, s3: S3_client, logger: Logger):
      file_repository = FileRepository(session)
+     project_repository = ProjectRepository(session)
      with tempfile.TemporaryDirectory() as tmp_dir:
             temp_dir = pathlib.Path(tmp_dir)
-            input_path = temp_dir / "input.mp4"
+
+            source_file = task_model.project.origin_file
+            input_type = resolve_file_type(source_file.file_type)
+
+            input_path = temp_dir / f"input{input_type.extension}"
             converted_path = temp_dir / "converted.wav"
             output_path = temp_dir / "output.json"
 
-            s3.fget_object(settings.minio_bucket, str(task_model.origin_file_id), str(input_path))
+
+            s3.fget_object(settings.minio_bucket, str(source_file.id), str(input_path))
 
             await extract_audio_and_convert(input_path, converted_path)
             for i in transcribe_audio(converted_path, output_path):
@@ -50,7 +58,7 @@ async def task_transcribe(task_model: Task, session: Session, s3: S3_client, log
 
             new_file = await file_repository.create(
                 "transcript.json",
-                "application/json",
+                FileTypes.text_json.internal,
                 task_model.user,
                 task_model.project,
                 task=task_model
@@ -59,19 +67,23 @@ async def task_transcribe(task_model: Task, session: Session, s3: S3_client, log
                 raise Exception("File not created")
             s3.fput_object(settings.minio_bucket, str(new_file.id), str(output_path))
 
+            await project_repository.add_transcription_file(task_model.project, new_file)
+
 async def task_summary(task_model: Task, session: Session, s3: S3_client, logger: Logger):
     file_repository = FileRepository(session)
+    project_repository = ProjectRepository(session)
     with tempfile.TemporaryDirectory() as tmp_dir:
         input_path = pathlib.Path(tmp_dir) / "input.json"
-        output_path = pathlib.Path(tmp_dir) / "output.txt"
+        output_path = pathlib.Path(tmp_dir) / "output.md"
+        transcript_file = task_model.project.transcription
 
-        s3.fget_object(settings.minio_bucket, str(task_model.origin_file_id), str(input_path))
+        s3.fget_object(settings.minio_bucket, str(transcript_file), str(input_path))
 
         create_summary(input_path, output_path)
 
         new_file = await file_repository.create(
             "summary.txt",
-            "text/plain",
+            FileTypes.text_md.internal,
             task_model.user,
             task_model.project,
             task=task_model
@@ -79,6 +91,8 @@ async def task_summary(task_model: Task, session: Session, s3: S3_client, logger
         if new_file is None:
             raise Exception("File not created")
         s3.fput_object(settings.minio_bucket, str(new_file.id), str(output_path))
+
+        await project_repository.add_summary_file(task_model.project, new_file)
 
 @broker.subscriber(working_queue[settings.task_type])
 @broker.publisher(RabbitQueuesToBack.done_task)
@@ -98,7 +112,10 @@ async def handle(task: TaskToAi,
        await task_transcribe(task_model, session, s3, logger)
     if settings.task_type == TaskType.summary:
         await task_summary(task_model, session, s3, logger)
-    
+
+    task_model.done = True
+    await session.flush()
+
     logger.info(f"Task {task.task_id} end in {time.time() - start_time:.2f} seconds")
     return TaskToBack(task_id=task.task_id,
                       done=True,
